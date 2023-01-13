0eb67e45faaf1f3ec878c9f37b5ba600ec6ad885
Rosa Jeon <94203128+CuriousR82@users.noreply.github.com>
2023-01-13 00:27:56 -0500

    Update Player.js

--- src/Player.js
+++ src/Player.js
@@ -1,10 +1,22 @@
-import { AnimationMixer, Mesh } from 'three';
+import { AnimationMixer, Mesh, BoxGeometry, MeshBasicMaterial } from 'three';
 import { cm1 } from './common';
 import { Stuff } from './Stuff';
 
 export class Player extends Stuff {
     constructor(info) {
         super(info);
+
+        this.width = 0.5;
+        this.height = 0.5;
+        this.depth = 0.5;
+
+        this.mesh = new Mesh(
+            new BoxGeometry(this.width, this.height, this.depth),
+            new MeshBasicMaterial({transparent: true, opacity: 0})
+        )
+        this.mesh.castShadow = true;
+        this.mesh.position.set(this.x, this.y, this.z);
+        cm1.scene.add(this.mesh);
 
         cm1.gltfLoader.load(
             '/models/squidgame.glb',
@@ -23,9 +35,13 @@ export class Player extends Stuff {
 				cm1.mixer = new AnimationMixer(this.modelMesh);
 				this.actions = [];
 				this.actions[0] = cm1.mixer.clipAction(this.modelMesh.animations[0]); // default
-				this.actions[1] = cm1.mixer.clipAction(this.modelMesh.animations[1]); // fall
+				this.actions[1] = cm1.mixer.clipAction(this.modelMesh.animations[1]); // jump
+                this.actions[1].repetitions = 1;
 				
 				this.actions[0].play();
+
+                this.setCannonBody();
+
             }
         )
 
