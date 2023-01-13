c04cac98a9d3ba0c2beefa3b46e373a35cf74f62
Rosa Jeon <94203128+CuriousR82@users.noreply.github.com>
2023-01-13 00:26:30 -0500

    Update Bar.js

--- src/Bar.js
+++ src/Bar.js
@@ -2,6 +2,7 @@ import { Mesh } from 'three';
 import { cm1, geo, mat } from './common';
 import { Stuff } from './Stuff';
 
+
 export class Bar extends Stuff {
     constructor(info) {
         super(info);
@@ -9,11 +10,17 @@ export class Bar extends Stuff {
         this.geometry = geo.bar;
         this.material = mat.bar;
 
+        this.width = this.geometry.parameters.width;
+        this.height = this.geometry.parameters.height;
+        this.depth = this.geometry.parameters.depth;
+
         this.mesh = new Mesh(this.geometry, this.material);
         this.mesh.position.set(this.x, this.y, this.z);
         this.mesh.castShadow = true;
         this.mesh.receiveShadow = true;
 
         cm1.scene.add(this.mesh);
+
+        this.setCannonBody();
     }
 }
